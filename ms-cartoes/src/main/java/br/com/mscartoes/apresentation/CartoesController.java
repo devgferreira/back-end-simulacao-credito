68e7c2dc7661162c5c5658a797b85bb7b839268f
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-09-26 15:51:24 -0300

    feat: criando a controller CartoesController

--- ms-cartoes/src/main/java/br/com/mscartoes/apresentation/CartoesController.java
+++ ms-cartoes/src/main/java/br/com/mscartoes/apresentation/CartoesController.java
@@ -1,29 +1,37 @@
 package br.com.mscartoes.apresentation;
 
 import br.com.mscartoes.application.dtos.CartaoDTO;
+import br.com.mscartoes.application.dtos.ClienteCartaoDTO;
+import br.com.mscartoes.application.dtos.response.CartoesPorClienteResponse;
 import br.com.mscartoes.application.interfaces.ICartaoService;
-import br.com.mscartoes.domain.model.CartaoSaveResponse;
-import org.apache.http.protocol.HTTP;
+import br.com.mscartoes.application.dtos.response.CartaoSaveResponse;
+import br.com.mscartoes.application.interfaces.IClienteCartaoService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/cartoes")
+@Slf4j
 public class CartoesController {
 
     private final ICartaoService _cartaoService;
+    private final IClienteCartaoService _clienteCartaoService;
 
     @Autowired
-    public CartoesController(ICartaoService cartaoService) {
+    public CartoesController(ICartaoService cartaoService, IClienteCartaoService clienteCartaoService) {
         _cartaoService = cartaoService;
+        _clienteCartaoService = clienteCartaoService;
     }
 
     @GetMapping
     public String status(){
+        log.info("Obetando o status do microservice de cartões");
         return "ok";
     }
 
@@ -33,9 +41,16 @@ public class CartoesController {
         CartaoSaveResponse cartaoSaveResponse = new CartaoSaveResponse(cartao);
         return new ResponseEntity<>(cartaoSaveResponse, HttpStatus.CREATED);
     }
-    @GetMapping("/{renda}")
-    public ResponseEntity<List<CartaoDTO>> getCartoesRendaAte(@PathVariable("renda") Long renda){
+    @GetMapping(params = "renda")
+    public ResponseEntity<List<CartaoDTO>> getCartoesRendaAte(@RequestParam("renda") Long renda){
         List<CartaoDTO> cartoes = _cartaoService.getCartoesRendaMenorIgual(renda);
         return ResponseEntity.ok(cartoes);
+    }
+    @GetMapping(params = "cpf")
+    public ResponseEntity<List<CartoesPorClienteResponse>> getCartoesByCliente(@RequestParam("cpf") String cpf){
+        List<ClienteCartaoDTO> clienteCartoes = _clienteCartaoService.listCartoesByCpf(cpf);
+        List<CartoesPorClienteResponse> resultList = clienteCartoes.stream().
+                map(CartoesPorClienteResponse::fromModel).toList();
+        return ResponseEntity.ok(resultList);
     }
 }
