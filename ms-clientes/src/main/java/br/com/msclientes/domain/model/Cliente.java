a08eb7c700fc79ed867d1aed981d9040b769f076
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-09-25 23:31:31 -0300

    feat: criando a entidade Cliente

---
+++ ms-clientes/src/main/java/br/com/msclientes/domain/model/Cliente.java
@@ -0,0 +1,20 @@
+package br.com.msclientes.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String cpf;
+    private String nome;
+    private Integer idade;
+}
