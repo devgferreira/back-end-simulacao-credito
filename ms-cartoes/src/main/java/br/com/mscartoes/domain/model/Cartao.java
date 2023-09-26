3e507fd958c999aec381c4068362bcfe5f4fa5ca
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-09-26 13:54:51 -0300

    feat: criando a entidade Cartao

---
+++ ms-cartoes/src/main/java/br/com/mscartoes/domain/model/Cartao.java
@@ -0,0 +1,22 @@
+package br.com.mscartoes.domain.model;
+
+import br.com.mscartoes.domain.enums.BandeiraCartao;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Cartao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    @Enumerated(EnumType.STRING)
+    private BandeiraCartao bandeira;
+    private BigDecimal renda;
+    private BigDecimal limiteBasico;
+}
