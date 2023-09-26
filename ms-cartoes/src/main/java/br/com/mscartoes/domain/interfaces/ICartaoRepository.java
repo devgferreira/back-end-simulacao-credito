ac3ec6670ecf5bb4d9eff25b2e3745b30708cacb
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-09-26 14:27:31 -0300

    feat: criando o contrato findByRendaLessThanEqual no ICartaoRepository

--- ms-cartoes/src/main/java/br/com/mscartoes/domain/interfaces/ICartaoRepository.java
+++ ms-cartoes/src/main/java/br/com/mscartoes/domain/interfaces/ICartaoRepository.java
@@ -1,7 +1,12 @@
 package br.com.mscartoes.domain.interfaces;
 
+import br.com.mscartoes.application.dtos.CartaoDTO;
 import br.com.mscartoes.domain.model.Cartao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface ICartaoRepository extends JpaRepository<Cartao, Long> {
+    List<Cartao> findByRendaLessThanEqual(BigDecimal rendaBigDecimal);
 }
