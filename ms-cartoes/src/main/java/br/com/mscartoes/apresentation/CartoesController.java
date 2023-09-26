c337162a9c55677952cb85d20521a2d63d062f9b
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-09-26 14:28:33 -0300

    feat: criando a controller CartoesController e implmentando o método cadastra

---
+++ ms-cartoes/src/main/java/br/com/mscartoes/apresentation/CartoesController.java
@@ -0,0 +1,34 @@
+package br.com.mscartoes.apresentation;
+
+import br.com.mscartoes.application.dtos.CartaoDTO;
+import br.com.mscartoes.application.interfaces.ICartaoService;
+import br.com.mscartoes.domain.model.CartaoSaveResponse;
+import org.apache.http.protocol.HTTP;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cartoes")
+public class CartoesController {
+
+    private final ICartaoService _cartaoService;
+
+    @Autowired
+    public CartoesController(ICartaoService cartaoService) {
+        _cartaoService = cartaoService;
+    }
+
+    @GetMapping
+    public String status(){
+        return "ok";
+    }
+
+    @PostMapping
+    public ResponseEntity<CartaoSaveResponse> cadastra(@RequestBody CartaoDTO cartaoDTO){
+        CartaoDTO cartao = _cartaoService.save(cartaoDTO);
+        CartaoSaveResponse cartaoSaveResponse = new CartaoSaveResponse(cartao);
+        return new ResponseEntity<>(cartaoSaveResponse, HttpStatus.CREATED);
+    }
+}
