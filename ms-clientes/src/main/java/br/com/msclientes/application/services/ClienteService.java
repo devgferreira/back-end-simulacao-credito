6914c3743f67f7d1de69b78af116b24d19de914d
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-09-25 23:31:18 -0300

    feat: criando a classe ClienteService

---
+++ ms-clientes/src/main/java/br/com/msclientes/application/services/ClienteService.java
@@ -0,0 +1,31 @@
+package br.com.msclientes.application.services;
+
+import br.com.msclientes.application.dtos.ClienteDTO;
+import br.com.msclientes.application.interfaces.IClienteService;
+import br.com.msclientes.domain.interfaces.IClienteRepository;
+import br.com.msclientes.domain.model.Cliente;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ClienteService implements IClienteService {
+
+    private final IClienteRepository _clienteRepository;
+    private final ModelMapper _modelMapper;
+
+    @Autowired
+    public ClienteService(IClienteRepository clienteRepository, ModelMapper modelMapper) {
+        _clienteRepository = clienteRepository;
+        _modelMapper = modelMapper;
+    }
+
+    @Override
+    public ClienteDTO save(ClienteDTO clienteDTO) {
+        Cliente cliente = _modelMapper.map(clienteDTO, Cliente.class);
+        return _modelMapper.map(_clienteRepository.save(cliente), ClienteDTO.class) ;
+    }
+
+    @Override
+    public ClienteDTO getByCPF(String cpf) {
+        return _modelMapper.map(_clienteRepository.findByCPF(cpf).orElse(new Cliente()), ClienteDTO.class);
+    }
+}
