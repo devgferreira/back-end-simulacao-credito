b5208bbfe39bd6b7510021ebdc6a7019bc2c14d9
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-09-28 15:10:46 -0300

    fix: criando o contrato solicitarEmissaoCartao na IAvaliadorCreditoService

--- ms-avaliador-credito/src/main/java/br/com/msavaliadorcredito/application/interfaces/IAvaliadorCreditoService.java
+++ ms-avaliador-credito/src/main/java/br/com/msavaliadorcredito/application/interfaces/IAvaliadorCreditoService.java
@@ -1,12 +1,17 @@
 package br.com.msavaliadorcredito.application.interfaces;
 
+import br.com.msavaliadorcredito.domian.model.dados.DadosSolicitacaoEmissaoCartao;
 import br.com.msavaliadorcredito.domian.model.dados.RetornoAvaliacaoCliente;
 import br.com.msavaliadorcredito.domian.model.cliente.SituacaoCliente;
+import br.com.msavaliadorcredito.domian.model.protocolo.ProtocoloSolicitacaoCartao;
 import br.com.msavaliadorcredito.infra.exceptions.DadosClienteNotFoundException;
 import br.com.msavaliadorcredito.infra.exceptions.ErroComunicacaoMicroserviceException;
+import br.com.msavaliadorcredito.infra.exceptions.ErroSolicitacaoCartaoException;
 
 public interface IAvaliadorCreditoService {
 
    RetornoAvaliacaoCliente realizarAvalicao(String cpf, Long renda) throws ErroComunicacaoMicroserviceException, DadosClienteNotFoundException;
    SituacaoCliente obterSituacaoCliente(String cpf) throws DadosClienteNotFoundException, ErroComunicacaoMicroserviceException;
+
+   ProtocoloSolicitacaoCartao solicitarEmissaoCartao(DadosSolicitacaoEmissaoCartao dados) throws ErroSolicitacaoCartaoException;
 }
