c59548f279e560882a27a1b0a53908f8bb776fec
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-09-27 22:46:03 -0300

    feat: criando mais um contrato na IAvaliadorCreditoService - realizarAvalicao

--- ms-avaliador-credito/src/main/java/br/com/msavaliadorcredito/application/interfaces/IAvaliadorCreditoService.java
+++ ms-avaliador-credito/src/main/java/br/com/msavaliadorcredito/application/interfaces/IAvaliadorCreditoService.java
@@ -1,9 +1,12 @@
 package br.com.msavaliadorcredito.application.interfaces;
 
+import br.com.msavaliadorcredito.domian.model.RetornoAvaliacaoCliente;
 import br.com.msavaliadorcredito.domian.model.SituacaoCliente;
 import br.com.msavaliadorcredito.infra.clients.exceptions.DadosClienteNotFoundException;
 import br.com.msavaliadorcredito.infra.clients.exceptions.ErroComunicacaoMicroserviceException;
 
 public interface IAvaliadorCreditoService {
+
+   RetornoAvaliacaoCliente realizarAvalicao(String cpf, Long renda) throws ErroComunicacaoMicroserviceException, DadosClienteNotFoundException;
    SituacaoCliente obterSituacaoCliente(String cpf) throws DadosClienteNotFoundException, ErroComunicacaoMicroserviceException;
 }
