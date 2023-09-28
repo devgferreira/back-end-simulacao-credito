2635f7616a27b5339d827f01522fa1462205053b
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-09-27 22:46:25 -0300

    feat: criando implementando o método realizarAvalicao na AvaliadorCreditoService

--- ms-avaliador-credito/src/main/java/br/com/msavaliadorcredito/application/service/AvaliadorCreditoService.java
+++ ms-avaliador-credito/src/main/java/br/com/msavaliadorcredito/application/service/AvaliadorCreditoService.java
@@ -1,9 +1,7 @@
 package br.com.msavaliadorcredito.application.service;
 
 import br.com.msavaliadorcredito.application.interfaces.IAvaliadorCreditoService;
-import br.com.msavaliadorcredito.domian.model.CartaoCliente;
-import br.com.msavaliadorcredito.domian.model.DadosCliente;
-import br.com.msavaliadorcredito.domian.model.SituacaoCliente;
+import br.com.msavaliadorcredito.domian.model.*;
 import br.com.msavaliadorcredito.infra.clients.CartoesControllerClient;
 import br.com.msavaliadorcredito.infra.clients.ClienteControllerClient;
 import br.com.msavaliadorcredito.infra.clients.exceptions.DadosClienteNotFoundException;
@@ -11,9 +9,12 @@ import br.com.msavaliadorcredito.infra.clients.exceptions.ErroComunicacaoMicrose
 import feign.FeignException;
 import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AvaliadorCreditoService implements IAvaliadorCreditoService {
@@ -26,15 +27,48 @@ public class AvaliadorCreditoService implements IAvaliadorCreditoService {
     }
 
     @Override
+    public RetornoAvaliacaoCliente realizarAvalicao(String cpf, Long renda)
+            throws ErroComunicacaoMicroserviceException, DadosClienteNotFoundException {
+        try {
+            DadosCliente dadosCliente = _clienteControllerClient.dadosCliente(cpf);
+            ResponseEntity<List<Cartao>> cartoesResponse = _cartoesControllerClient.getCartoesRendaAte(renda);
+            List<Cartao> cartoes = cartoesResponse.getBody();
+            List<CartaoAprovado> listaCartoesAprovados = cartoes.stream().map(cartao -> {
+
+                BigDecimal limiteLiberado = cartao.getLimiteBasico();
+                BigDecimal idadeBD = BigDecimal.valueOf(dadosCliente.getIdade());
+                var fator = idadeBD.divide(BigDecimal.valueOf(10));
+                BigDecimal limiteAprovado = fator.multiply(limiteLiberado);
+
+                CartaoAprovado aprovado = new CartaoAprovado();
+                aprovado.setCartao(cartao.getNome());
+                aprovado.setBandeira(cartao.getBandeira());
+                aprovado.setLimiteAprovado(limiteAprovado);
+
+                return aprovado;
+            }).collect(Collectors.toList());
+
+            return new RetornoAvaliacaoCliente(listaCartoesAprovados);
+
+        }catch (FeignException.FeignClientException ex){
+            int status = ex.status();
+            if(HttpStatus.NOT_FOUND.value() == status){
+                throw new DadosClienteNotFoundException();
+            }
+            throw new ErroComunicacaoMicroserviceException(ex.getMessage(), status);
+        }
+    }
+
+    @Override
     public SituacaoCliente obterSituacaoCliente(String cpf)
             throws DadosClienteNotFoundException, ErroComunicacaoMicroserviceException {
         try {
             DadosCliente dadosCliente = _clienteControllerClient.dadosCliente(cpf);
-            List<CartaoCliente> cartoesCliente = _cartoesControllerClient.getCartoesByCliente(cpf);
+            ResponseEntity<List<CartaoCliente>> cartoesCliente = _cartoesControllerClient.getCartoesByCliente(cpf);
 
             return SituacaoCliente.builder()
                     .cliente(dadosCliente)
-                    .cartoes(cartoesCliente)
+                    .cartoes(cartoesCliente.getBody())
                     .build();
         }catch (FeignException.FeignClientException ex){
            int status = ex.status();
