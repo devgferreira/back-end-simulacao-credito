20411476ec950340f5a88e5c05e4625e4ac226b0
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2023-11-17 11:52:56 -0300

    test: criando o ClienteConstatns para implementar nos testes

---
+++ ms-clientes/src/test/java/br/com/msclientes/common/ClienteConstatns.java
@@ -0,0 +1,8 @@
+package br.com.msclientes.common;
+
+import br.com.msclientes.application.dtos.ClienteDTO;
+
+public class ClienteConstatns {
+    public  static final ClienteDTO CLIENTEDTO_VALIDO = new ClienteDTO("78930792073");
+    public  static final ClienteDTO CLIENTEDTO_INVALIDO = new ClienteDTO("0000000000");
+}
