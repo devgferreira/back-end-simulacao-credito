37adbe2b71f7b8f2df5a006842699d44a36716f8
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2023-11-17 11:26:13 -0300

    feat: tirando o id do Response

--- ms-clientes/src/main/java/br/com/msclientes/domain/model/ClienteSaveResponse.java
+++ ms-clientes/src/main/java/br/com/msclientes/domain/model/ClienteSaveResponse.java
@@ -5,13 +5,11 @@ import lombok.Data;
 
 @Data
 public class ClienteSaveResponse {
-    private Long id;
     private String cpf;
     private String nome;
     private Integer idade;
 
     public ClienteSaveResponse(ClienteDTO clienteDTO){
-        id = clienteDTO.getId();
         cpf = clienteDTO.getCpf();
         nome = clienteDTO.getNome();
         idade = clienteDTO.getIdade();
