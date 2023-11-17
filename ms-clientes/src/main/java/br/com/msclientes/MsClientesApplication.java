fc56375f76d77e80e5e3ce2db0861d18e9849257
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2023-11-17 11:29:16 -0300

    feat: adicionando a anotação @OpenAPIDefinition

--- ms-clientes/src/main/java/br/com/msclientes/MsClientesApplication.java
+++ ms-clientes/src/main/java/br/com/msclientes/MsClientesApplication.java
@@ -1,11 +1,13 @@
 package br.com.msclientes;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@OpenAPIDefinition
 public class MsClientesApplication {
 
 	public static void main(String[] args) {
