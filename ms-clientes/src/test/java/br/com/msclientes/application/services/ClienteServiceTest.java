2a05290826e0c70c336a047d1b730d3b0d0e9f90
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2023-11-18 20:10:41 -0300

    test: criando o test: getByCPF_ComOCpfValido_RetornandoClienteDTO no ClienteServiceTest

--- ms-clientes/src/test/java/br/com/msclientes/application/services/ClienteServiceTest.java
+++ ms-clientes/src/test/java/br/com/msclientes/application/services/ClienteServiceTest.java
@@ -1,9 +1,13 @@
 package br.com.msclientes.application.services;
 
 import br.com.msclientes.application.dtos.ClienteDTO;
+
+import static br.com.msclientes.common.ClienteConstatns.CLIENTEDTO_INVALIDO;
 import static br.com.msclientes.common.ClienteConstatns.CLIENTEDTO_VALIDO;
 import br.com.msclientes.domain.interfaces.IClienteRepository;
 import br.com.msclientes.domain.model.Cliente;
+import br.com.msclientes.infra.exceptions.ClienteJaExisteExeception;
+import br.com.msclientes.infra.exceptions.CpfInvalidoExeception;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -11,7 +15,9 @@ import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
-import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -29,19 +35,32 @@ class ClienteServiceTest {
     void criarCliente_ComOCpfNaoUsado_RetornandoCLienteDTO() {
 
         Cliente cliente = _mapper.map(CLIENTEDTO_VALIDO, Cliente.class);
-        cliente.setId(1L);
         when(_clienteRepository.save(any())).thenReturn(cliente);
 
-        ClienteDTO result = _clienteService.save(CLIENTEDTO_VALIDO);
+        ClienteDTO result = _clienteService.criarCliente(CLIENTEDTO_VALIDO);
 
-        assertEquals(CLIENTEDTO_VALIDO.getCpf(), cliente.getCpf());
+        assertEquals(CLIENTEDTO_VALIDO, result);
     }
 
     @Test
-    void getByCPF() {
+    void criarCliente_ComOCpUsado_RetornandoThrowsClienteJaExisteExeception() {
+
+        Cliente cliente = _mapper.map(CLIENTEDTO_VALIDO, Cliente.class);
+        cliente.setId(1L);
+
+        when(_clienteRepository.findByCpf(cliente.getCpf())).thenReturn(Optional.of(cliente));
+
+        assertThrows(ClienteJaExisteExeception.class,
+                () -> _clienteService.criarCliente(CLIENTEDTO_VALIDO));
     }
 
     @Test
-    void validator() {
+    void getByCPF_ComOCpfValido_RetornandoClienteDTO () {
+        Cliente cliente = _mapper.map(CLIENTEDTO_VALIDO, Cliente.class);
+        when(_clienteRepository.findByCpf(cliente.getCpf())).thenReturn(Optional.of(cliente));
+        ClienteDTO result = _clienteService.getByCPF(cliente.getCpf());
+        assertEquals(CLIENTEDTO_VALIDO, result);
     }
+
+
 }
