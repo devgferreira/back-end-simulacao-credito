a7f92e7e7d41c7b4dd6d523696c3d77b4f318509
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2023-11-19 22:45:39 -0300

    feat: criando a classe: ExceptionResponse

---
+++ ms-cartoes/src/main/java/br/com/mscartoes/infra/exceptions/ExceptionResponse.java
@@ -0,0 +1,19 @@
+package br.com.mscartoes.infra.exceptions;
+
+import br.com.mscartoes.domain.enums.ErrorCodes;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ExceptionResponse {
+    private final String code;
+    private final String message;
+
+    public ExceptionResponse(final ErrorCodes errorCode, String details) {
+        this.code = errorCode.name();
+        this.message = errorCode.getMessage();
+    }
+
+
+}
