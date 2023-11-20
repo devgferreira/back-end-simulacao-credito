5b635614d636db9ba68eb84e5a1538f6f073612b
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2023-11-19 22:46:51 -0300

    feat: tratando os erros na service: CartaoService

--- ms-cartoes/src/main/java/br/com/mscartoes/application/services/CartaoService.java
+++ ms-cartoes/src/main/java/br/com/mscartoes/application/services/CartaoService.java
@@ -2,8 +2,12 @@ package br.com.mscartoes.application.services;
 
 import br.com.mscartoes.application.dtos.CartaoDTO;
 import br.com.mscartoes.application.interfaces.ICartaoService;
+import br.com.mscartoes.domain.enums.ErrorCodes;
 import br.com.mscartoes.domain.interfaces.ICartaoRepository;
 import br.com.mscartoes.domain.model.Cartao;
+import br.com.mscartoes.infra.constants.ErrorConstants;
+import br.com.mscartoes.infra.exceptions.CartaoNaoEncontradoExeception;
+import br.com.mscartoes.infra.exceptions.ExceptionResponse;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -39,6 +43,9 @@ public class CartaoService implements ICartaoService {
     public List<CartaoDTO> getCartoesRendaMenorIgual(Long renda){
         BigDecimal rendaBigDecimal = BigDecimal.valueOf(renda);
         List<Cartao> cartoes = _cartaoRepository.findByRendaLessThanEqual(rendaBigDecimal);
+        if(cartoes == null){
+            throw new CartaoNaoEncontradoExeception(new ExceptionResponse(ErrorCodes.CARTAO_NAO_ENCONTRADO, ErrorConstants.CARTAO_NAO_ENCONTRADO));
+        }
         return cartoes.stream()
                 .map(entity -> _modelMapper.map(entity, CartaoDTO.class))
                 .collect(Collectors.toList());
