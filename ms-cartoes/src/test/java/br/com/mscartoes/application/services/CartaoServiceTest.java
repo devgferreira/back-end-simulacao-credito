844029039b65ff5e45987aa930dde4d5b062cfd3
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2023-11-21 17:06:19 -0300

    test: criando o test: criarCartao_ComOCartaoValido_RetoranandoCartao

---
+++ ms-cartoes/src/test/java/br/com/mscartoes/application/services/CartaoServiceTest.java
@@ -0,0 +1,44 @@
+package br.com.mscartoes.application.services;
+
+import br.com.mscartoes.application.dtos.CartaoDTO;
+import br.com.mscartoes.domain.interfaces.ICartaoRepository;
+import br.com.mscartoes.domain.model.Cartao;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import static br.com.mscartoes.common.CartaoConstants.CARTAO_DTO_VALIDO;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class CartaoServiceTest {
+    @InjectMocks
+    private CartaoService _cartaoService;
+    @Spy
+    private ModelMapper _mapper;
+    @Mock
+    private ICartaoRepository _cartaoRepository;
+    @Test
+    void criarCartao_ComOCartaoValido_RetoranandoCartao(){
+        Cartao cartao = _mapper.map(CARTAO_DTO_VALIDO, Cartao.class);
+        when(_cartaoRepository.save(any())).thenReturn(cartao);
+        CartaoDTO result = _cartaoService.criarCartao(CARTAO_DTO_VALIDO);
+
+        assertEquals(CARTAO_DTO_VALIDO, result);
+
+    }
+
+
+}
