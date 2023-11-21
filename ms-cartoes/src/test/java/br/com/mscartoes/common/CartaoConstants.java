b26e8f1da3ba4a214a243b096f9ae650df01aa86
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2023-11-21 17:04:09 -0300

    test: criando a constansts: CartaoConstants

---
+++ ms-cartoes/src/test/java/br/com/mscartoes/common/CartaoConstants.java
@@ -0,0 +1,8 @@
+package br.com.mscartoes.common;
+
+import br.com.mscartoes.application.dtos.CartaoDTO;
+
+public class CartaoConstants {
+
+    public static final CartaoDTO CARTAO_DTO_VALIDO = new CartaoDTO("Cartão");
+}
