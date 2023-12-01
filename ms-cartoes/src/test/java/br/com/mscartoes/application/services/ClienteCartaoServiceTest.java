40a46947c2d45207d50ea0659ace98cd332345f6
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2023-11-30 21:04:26 -0300

    test: criando o teste: listCartoesByCpf_ComCpfValido_RetornandoListaCartoes

--- ms-cartoes/src/test/java/br/com/mscartoes/application/services/ClienteCartaoServiceTest.java
+++ ms-cartoes/src/test/java/br/com/mscartoes/application/services/ClienteCartaoServiceTest.java
@@ -1,6 +1,11 @@
 package br.com.mscartoes.application.services;
 
+import br.com.mscartoes.application.dtos.ClienteCartaoDTO;
 import br.com.mscartoes.domain.interfaces.IClienteCartaoRepository;
+import br.com.mscartoes.domain.model.Cartao;
+import br.com.mscartoes.domain.model.ClienteCartao;
+import br.com.mscartoes.infra.exceptions.CartoesNaoEncontradosExeception;
+import br.com.mscartoes.infra.exceptions.ClienteNaoEncontradoExeception;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -9,12 +14,13 @@ import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
+import static br.com.mscartoes.common.ClienteCartaoConstants.CLIENTE_CARTAO_DTO;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 @ExtendWith(MockitoExtension.class)
 class ClienteCartaoServiceTest {
     @InjectMocks
@@ -24,6 +30,21 @@ class ClienteCartaoServiceTest {
     @Mock
     private IClienteCartaoRepository _clienteCartaoRepository;
     @Test
-    void listCartoesByCpf() {
+    void listCartoesByCpf_ComCpfValido_RetornandoListaCartoes() {
+        String cpf = "12345678901";
+
+        ClienteCartao clienteCartao = _mapper.map(CLIENTE_CARTAO_DTO, ClienteCartao.class);
+        List<ClienteCartao> clienteCartaoList = new ArrayList<>();
+        clienteCartaoList.add(clienteCartao);
+        when(_clienteCartaoRepository.findByCpf(cpf)).thenReturn(clienteCartaoList);
+
+        List<ClienteCartaoDTO> result = _clienteCartaoService.listCartoesByCpf(cpf);
+        assertNotNull(result);
+        assertEquals(clienteCartaoList.size(), result.size());
+        verify(_clienteCartaoRepository, times(1)).findByCpf(cpf);
     }
+
+
+
+
 }
