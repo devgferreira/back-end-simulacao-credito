a71a9e643c4afeab4615e5da3a51c2a122384dab
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2023-12-02 13:00:54 -0300

    feat: criando a constant ClienteCartaoDTOConstants

--- ms-cartoes/src/test/java/br/com/mscartoes/common/ClienteCartaoDTOConstants.java
+++ ms-cartoes/src/test/java/br/com/mscartoes/common/ClienteCartaoDTOConstants.java
@@ -1,10 +1,9 @@
 package br.com.mscartoes.common;
 
-import br.com.mscartoes.application.dtos.CartaoDTO;
 import br.com.mscartoes.application.dtos.ClienteCartaoDTO;
-import br.com.mscartoes.domain.model.ClienteCartao;
 
-public class ClienteCartaoConstants {
+
+public class ClienteCartaoDTOConstants {
 
     public static final ClienteCartaoDTO CLIENTE_CARTAO_DTO = new ClienteCartaoDTO("12345678901");
 }
