101f5e9df3a6719b6c590417b7bb5186809bcbf9
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2023-12-02 13:14:18 -0300

    feat: criando a constants: CARTAO_DTO_RENDA_VALIDO no CartaoDTOConstants

--- ms-cartoes/src/test/java/br/com/mscartoes/common/CartaoDTOConstants.java
+++ ms-cartoes/src/test/java/br/com/mscartoes/common/CartaoDTOConstants.java
@@ -2,7 +2,10 @@ package br.com.mscartoes.common;
 
 import br.com.mscartoes.application.dtos.CartaoDTO;
 
+import java.math.BigDecimal;
+
 public class CartaoDTOConstants {
 
     public static final CartaoDTO CARTAO_DTO_VALIDO = new CartaoDTO("Cartão");
+    public static final CartaoDTO CARTAO_DTO_RENDA_VALIDO = new CartaoDTO(BigDecimal.valueOf(1000));
 }
