86ca3c099e947aeddb5b4c39a107d40cee4f9ced
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2023-12-02 13:01:25 -0300

    feat: Mudando o nome do CartaoConstants para CartaoDTOConstants

--- ms-cartoes/src/test/java/br/com/mscartoes/common/CartaoDTOConstants.java
+++ ms-cartoes/src/test/java/br/com/mscartoes/common/CartaoDTOConstants.java
@@ -2,7 +2,7 @@ package br.com.mscartoes.common;
 
 import br.com.mscartoes.application.dtos.CartaoDTO;
 
-public class CartaoConstants {
+public class CartaoDTOConstants {
 
     public static final CartaoDTO CARTAO_DTO_VALIDO = new CartaoDTO("Cartão");
 }
